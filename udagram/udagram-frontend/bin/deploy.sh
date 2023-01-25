5e6d5df40e22a9a9586af13b7905db5e7a0a98b7
mai907 <59649904+mai907@users.noreply.github.com>
2023-01-25 08:47:29 +0300

    Update deploy.sh
    add env-variable into deploy.sh

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,3 @@
 aws s3 cp --recursive --acl public-read ./www s3://myawsbucket-75139724085/
 aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://myawsbucket-75139724085/
+eb setenv AWS_REGION=$AWS_REGION PORT=$PORT AWS_PROFILE=$AWS_PROFILE API_HOST=$API_HOST 
